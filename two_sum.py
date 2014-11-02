bcc7494155b26eaedf36b201eab339407c5bcc3e
Charlie <xinzhou918@gmail.com>
2014-11-02 10:55:52 -0500

    try to improve two_sum.py

--- two_sum.py
+++ two_sum.py
@@ -10,24 +10,50 @@
 # Output: index1=1, index2=2
 #
 #
+import unittest
 
 
 class Solution:
     # @return a tuple, (index1, index2)
 
     def twoSum(self, num, target):
+        """ use extra space """
         dic = {}
         for k, v in enumerate(num):
             if target - v in dic:
                 return dic[target - v], k + 1
             dic[v] = k + 1
 
+    # def twoSum(self, num, target):
+    #     """
+    #     need extra space anyway
+    #     and this is n log n because of it's not sorted
+    #     """
+    #     ivlist = [(v, i + 1) for i, v in enumerate(num)]
+    #     ivlist.sort(key=lambda v: v[0])
+    #     s, e = 0, len(num) - 1
+    #     while s < e:
+    #         tmp = ivlist[s][0] + ivlist[e][0]
+    #         if tmp == target:
+    #             return ivlist[s][1], ivlist[e][1]
+    #         elif tmp > target:
+    #             e -= 1
+    #         elif tmp < target:
+    #             s += 1
 
-s = Solution()
-print s.twoSum([], 9)
-print s.twoSum([0, 0], 0)
-print s.twoSum([3, 2, 4], 6)
-print s.twoSum([-1, 1], 0)
-print s.twoSum([2, 7, 11, 15], 100)
-print s.twoSum([2, 7, 11, 15], 9)
-print s.twoSum([2, 7, 11, 15], 9)
+
+class Test(unittest.TestCase):
+
+    def setUp(self):
+        self.s = Solution()
+
+    def test(self):
+        self.assertIsNone(self.s.twoSum([], 9))
+        self.assertIsNone(self.s.twoSum([2, 7, 11, 15], 100))
+        self.assertEqual(self.s.twoSum([0, 0], 0), (1, 2))
+        self.assertEqual(self.s.twoSum([3, 2, 4], 6), (2, 3))
+        self.assertEqual(self.s.twoSum([-1, 1], 0), (1, 2))
+        self.assertEqual(self.s.twoSum([2, 7, 11, 15], 9), (1, 2))
+
+if __name__ == '__main__':
+    unittest.main()
