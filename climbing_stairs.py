82b80cc00cbba3e14ade775626be739d8587ea0e
Charlie <xinzhou918@gmail.com>
2014-11-02 22:35:02 -0500

    add climb stairs, fib seq

--- climbing_stairs.py
+++ climbing_stairs.py
@@ -1,15 +1,36 @@
 # coding=utf-8
 # AC Rate: 33.9%
 # SOURCE URL: https://oj.leetcode.com/problems/climbing-stairs/
-# 
+#
 # You are climbing a stair case. It takes n steps to reach to the top.
 # Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
-# 
-# 
+#
+#
+import unittest
 
 
 class Solution:
     # @param n, an integer
     # @return an integer
     def climbStairs(self, n):
-        
+        """
+        this should be able to use dp
+        """
+        a, b = 1, 1
+        for i in range(n-1):
+            a, b = a+b, a
+        return a
+
+
+class Test(unittest.TestCase):
+
+    def setUp(self):
+        self.s = Solution()
+
+    def test(self):
+        self.assertEqual(self.s.climbStairs(2), 2)
+        self.assertEqual(self.s.climbStairs(3), 3)
+        self.assertEqual(self.s.climbStairs(4), 5)
+
+if __name__ == '__main__':
+    unittest.main()
