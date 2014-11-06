073e4486d84a25423a9a62a62660d26283d67794
Charlie <xinzhou918@gmail.com>
2014-11-06 11:50:11 -0500

    add helper function to load tree from leetcode string add n queue and binary tree inorder with recursion

--- binary_tree_inorder_traversal.py
+++ binary_tree_inorder_traversal.py
@@ -1,31 +1,33 @@
 # coding=utf-8
 # AC Rate: 35.6%
 # SOURCE URL: https://oj.leetcode.com/problems/binary-tree-inorder-traversal/
-# 
+#
 # Given a binary tree, return the inorder traversal of its nodes' values.
-# 
+#
 # For example:
 # Given binary tree {1,#,2,3},
-# 
+#
 #    1
 #     \
 #      2
 #     /
 #    3
-# 
-# 
-# 
+#
+#
+#
 # return [1,3,2].
-# 
+#
 # Note: Recursive solution is trivial, could you do it iteratively?
-# confused what "{1,#,2,3}" means? > read more on how binary tree is serialized on OJ.
+# confused what "{1,#,2,3}" means? > read more on how binary tree is serialized
+# on OJ.
 # OJ's Binary Tree Serialization:
-# 
-# The serialization of a binary tree follows a level order traversal, where '#' signifies a path terminator where no node exists below.
-# 
-# 
+#
+# The serialization of a binary tree follows a level order traversal, where '#'
+# signifies a path terminator where no node exists below.
+#
+#
 # Here's an example:
-# 
+#
 #    1
 #   / \
 #  2   3
@@ -33,11 +35,11 @@
 #    4
 #     \
 #      5
-# 
-# The above binary tree is serialized as "{1,2,3,#,#,4,#,#,5}". 
-# 
-# 
-# 
+#
+# The above binary tree is serialized as "{1,2,3,#,#,4,#,#,5}".
+#
+#
+#
 
 
 # Definition for a  binary tree node
@@ -46,9 +48,36 @@
 #         self.val = x
 #         self.left = None
 #         self.right = None
+import unittest
+
 
 class Solution:
     # @param root, a tree node
     # @return a list of integers
+
     def inorderTraversal(self, root):
-        
+        return self.inorderTraversalHelper(root)
+
+    def inorderTraversalHelper(self, root):
+        res = []
+        if not root:
+            return res
+        res += self.inorderTraversalHelper(root.left)
+        res += [root.val]
+        res += self.inorderTraversalHelper(root.right)
+        return res
+
+
+class Test(unittest.TestCase):
+
+    def test(self):
+        from helper.tree import Tree
+        t = Tree()
+        t.load_leetcode_string('{1,2,3,#,#,4,#,#,5}')
+        s = Solution()
+        self.assertEqual(s.inorderTraversal(t.head),
+                         [2, 1, 4, 5, 3])
+
+
+if __name__ == '__main__':
+    unittest.main()
--- n_queens.py
+++ n_queens.py
@@ -1,30 +1,76 @@
 # coding=utf-8
 # AC Rate: 25.9%
 # SOURCE URL: https://oj.leetcode.com/problems/n-queens/
-# 
+#
 # The n-queens puzzle is the problem of placing n queens on an n×n chessboard such that no two queens attack each other.
-# 
+#
 # Given an integer n, return all distinct solutions to the n-queens puzzle.
 # Each solution contains a distinct board configuration of the n-queens' placement, where 'Q' and '.' both indicate a queen and an empty space respectively.
 # For example,
 # There exist two distinct solutions to the 4-queens puzzle:
-# 
+#
 # [
 #  [".Q..",  // Solution 1
 #   "...Q",
 #   "Q...",
 #   "..Q."],
-# 
+#
 #  ["..Q.",  // Solution 2
 #   "Q...",
 #   "...Q",
 #   ".Q.."]
 # ]
-# 
-# 
+#
+#
+import unittest
 
 
 class Solution:
     # @return a list of lists of string
+
+    def dfsHelper(self, col, res):
+        if col >= self.n:
+            res.append(self.print_board())
+            return
+        for i in range(self.n):
+            if not self.is_attack(col, i):
+                self.board[i][col] = 1
+                self.dfsHelper(col + 1, res)
+                self.board[i][col] = 0
+
+    def is_attack(self, col, i):
+        for col_ahead in range(col):
+            if self.board[i][col_ahead] == 1:
+                return True
+            col_diff = col - col_ahead
+            if i-col_diff >= 0 and self.board[i-col_diff][col_ahead] == 1:
+                return True
+            if i+col_diff < self.n and self.board[i+col_diff][col_ahead] == 1:
+                return True
+        return False
+
+    def print_board(self):
+        res = []
+        for row in self.board:
+            res.append(''.join(['Q' if b else '.' for b in row]))
+        return res
+
     def solveNQueens(self, n):
-        
+        self.n = n
+        self.board = [[0] * n for i in range(n)]
+        res = []
+        self.dfsHelper(0, res)
+        return res
+
+
+class Test(unittest.TestCase):
+
+    def test(self):
+        s = Solution()
+        self.assertEqual(s.solveNQueens(4),
+                         [['..Q.', 'Q...', '...Q', '.Q..'],
+                          ['.Q..', '...Q', 'Q...', '..Q.']])
+
+
+if __name__ == '__main__':
+    unittest.main()
