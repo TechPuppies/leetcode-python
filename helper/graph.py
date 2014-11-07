a904d7b479dda0d215746c1934ab588e4f36923f
Charlie <xinzhou918@gmail.com>
2014-11-07 00:15:44 -0500

    fix graph with reverse neighbors

--- helper/graph.py
+++ helper/graph.py
@@ -19,9 +19,12 @@ class Graph:
             self.V[row[0]] = UndirectedGraphNode(row[0])
 
         for row in matrix:
+            j = row[0]
             for i in row[1:]:
-                self.V[row[0]].neighbors.append(self.V[i])
-
+                if self.V[i] not in self.V[j].neighbors:
+                    self.V[j].neighbors.append(self.V[i])
+                if self.V[j] not in self.V[i].neighbors:
+                    self.V[i].neighbors.append(self.V[j])
         return self
 
     def load_leetcode_string(self, lc_str):
@@ -41,23 +44,22 @@ class Test(unittest.TestCase):
         self.g.load_matrix([[0, 1, 2], [1, 2], [2, 2]])
         self.assertItemsEqual(self.g.V.keys(), [0, 1, 2])
         self.assertItemsEqual(self.g.V[0].neighbors,
-                          [self.g.V[1], self.g.V[2]])
+                              [self.g.V[1], self.g.V[2]])
         self.assertItemsEqual(self.g.V[1].neighbors,
-                          [self.g.V[2]])
+                              [self.g.V[0], self.g.V[2]])
         self.assertItemsEqual(self.g.V[2].neighbors,
-                          [self.g.V[2]])
+                              self.g.V.values())
 
     def test2(self):
         self.g.load_leetcode_string('{0,1,2#1,2#2,2}')
         self.assertItemsEqual(self.g.V.keys(), ['0', '1', '2'])
         self.assertItemsEqual(self.g.V['0'].neighbors,
-                          [self.g.V['1'], self.g.V['2']])
+                              [self.g.V['1'], self.g.V['2']])
         self.assertItemsEqual(self.g.V['1'].neighbors,
-                          [self.g.V['2']])
+                              [self.g.V['2'], self.g.V['0']])
         self.assertItemsEqual(self.g.V['2'].neighbors,
-                          [self.g.V['2']])
+                              self.g.V.values())
 
 
 if __name__ == '__main__':
     unittest.main()
-
