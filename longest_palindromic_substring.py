1ce2687d9398ba54234dfbc1a43aa82c70db17d7
Charlie <xinzhou918@gmail.com>
2014-11-09 20:10:10 -0500

    refactor longest palindromic substring

--- longest_palindromic_substring.py
+++ longest_palindromic_substring.py
@@ -2,34 +2,49 @@
 # AC Rate: 20.7%
 # SOURCE URL: https://oj.leetcode.com/problems/longest-palindromic-substring/
 #
-# Given a string S, find the longest palindromic substring in S. You may assume that the maximum length of S is 1000, and there exists one unique longest palindromic substring.
+# Given a string S, find the longest palindromic substring in S. You may assume
+# that the maximum length of S is 1000, and there exists one unique longest
+# palindromic substring.
 #
+import unittest
 
 
 class Solution:
     # @return a string
+
+    def oddPalindrome(self, s, k):
+        i = 0
+        while k - i >= 0 and k + i < len(s) and s[k - i] == s[k + i]:
+            i += 1
+        return s[k - i + 1:k + i]
+
+    def evenPalindrome(self, s, k):
+        i = 0
+        while k - i >= 0 and k + 1 + i < len(s) and s[k - i] == s[k + 1 + i]:
+            i += 1
+        return s[k - i + 1:k + 1 + i]
+
     def longestPalindrome(self, s):
-        # O(n**2)
-        best, match = 0, ''
+        best = ''
         for k in range(len(s)):
-            i = 0
-            while k - i >= 0 and k + i < len(s) and s[k - i] == s[k + i]:
-                i += 1
-            if 2 * i - 1 > best:
-                best = 2 * i - 1
-                match = s[k-i+1:k+i]
+            odd = self.oddPalindrome(s, k)
+            even = self.evenPalindrome(s, k)
+            if len(odd) > len(best):
+                best = odd
+            if len(even) > len(best):
+                best = even
+        return best
 
-            i = 0
-            while k - i >= 0 and k + 1 + i < len(s) and s[k-i] == s[k+1+i]:
-                i += 1
-            if 2 * i > best:
-                best = 2 * i
-                match = s[k-i+1:k+i+1]
-        return match
 
-s = Solution()
-print s.longestPalindrome('abcbaaddaa')
+class Test(unittest.TestCase):
 
+    def test(self):
+        s = Solution()
+        self.assertEqual(s.longestPalindrome('abcbaaddaa'), 'aaddaa')
+
+
+if __name__ == '__main__':
+    unittest.main()
 
 # this is n ** 2
 # there is O(n) alg
