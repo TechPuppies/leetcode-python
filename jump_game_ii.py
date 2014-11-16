29ff0d2de82a7d69ff84565b728acb3c79924be3
Charlie <xinzhou918@gmail.com>
2014-11-16 00:07:17 -0500

    revert jump game ii

--- jump_game_ii.py
+++ jump_game_ii.py
@@ -3,10 +3,12 @@
 # SOURCE URL: https://oj.leetcode.com/problems/jump-game-ii/
 #
 #
-# Given an array of non-negative integers, you are initially positioned at the first index of the array.
+# Given an array of non-negative integers, you are initially positioned
+# at the first index of the array.
 #
 #
-# Each element in the array represents your maximum jump length at that position.
+# Each element in the array represents your maximum jump length at that
+# position.
 #
 #
 # Your goal is to reach the last index in the minimum number of jumps.
@@ -16,30 +18,38 @@
 # Given array A = [2,3,1,1,4]
 #
 #
-# The minimum number of jumps to reach the last index is 2. (Jump 1 step from index 0 to 1, then 3 steps to the last index.)
+# The minimum number of jumps to reach the last index is 2.
+# (Jump 1 step from index 0 to 1, then 3 steps to the last index.)
 #
 #
 import unittest
-
 
 class Solution:
     # @param A, a list of integers
     # @return an integer
     def jump(self, A):
-        # use inf present not possible
-        jumpSteps = [float('inf')] * len(A)
-        # target to target == 0
-        jumpSteps[-1] = 0
-        for i in xrange(len(A) - 2, -1, -1):
-            jumpSteps[i] = 1 + min([jumpSteps[i + j] for j in xrange(1, A[i] + 1)])
-        return jumpSteps[0]
+        ret = 0
+        last = 0
+        curr = 0
+        for i in range(len(A)):
+            if i > last:
+                # if not last one and can't go further
+                if (curr == last) and (last < len(A)-1):
+                    return -1   # never reach the last one
+                last = curr
+                ret += 1
+            curr = max(curr, i+A[i])
+        return ret
 
 
 class Test(unittest.TestCase):
 
     def test(self):
         s = Solution()
-        self.assertEqual(s.jump([2,3,1,1,4]), 2)
+        self.assertEqual(s.jump([0]), 0)
+        self.assertEqual(s.jump([2, 1]), 1)
+        self.assertEqual(s.jump([1, 2, 0, 1]), 2)
+        self.assertEqual(s.jump([2, 3, 1, 1, 4]), 2)
         self.assertEqual(s.jump([1 for i in range(250000)]), 249999)
 
 if __name__ == '__main__':
