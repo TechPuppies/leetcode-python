d3b5ef8ac49ec72213ad7d189f10a2818d7f0a89
X. Zhou <xz859@nyu.edu>
2015-01-06 16:26:46 -0500

    Update jump_game_ii.py

--- jump_game_ii.py
+++ jump_game_ii.py
@@ -28,16 +28,12 @@ class Solution:
     # @param A, a list of integers
     # @return an integer
     def jump(self, A):
-        ret = 0
-        last = 0
-        curr = 0
+        ret, last, curr = 0, 0, 0
         for i in range(len(A)):
-            if i > last:
-                # if not last one and can't go further
+            if i > last:  # if not last one and can't go further
                 if (curr == last) and (last < len(A)-1):
                     return -1   # never reach the last one
-                last = curr
-                ret += 1
+                last, ret = curr, ret + 1
             curr = max(curr, i+A[i])
         return ret
 
